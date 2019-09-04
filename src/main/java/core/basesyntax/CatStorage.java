c213ba77fbb962d5c09e304da850339538d4771e
joej0e <36197513+joej0e@users.noreply.github.com>
2019-09-05 00:23:11 +0300

    Add files via upload

---
+++ src/main/java/core/basesyntax/CatStorage.java
@@ -0,0 +1,10 @@
+package core.basesyntax.db;
+
+import core.basesyntax.model.Cat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatStorage {
+    public static final List<Cat> cats = new ArrayList<>();
+}
---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,31 @@
+package core.basesyntax;
+
+import core.basesyntax.controller.CatHandler;
+import core.basesyntax.controller.ConsoleHandler;
+import core.basesyntax.dao.BetDao;
+import core.basesyntax.dao.BetDaoImpl;
+import core.basesyntax.dao.CatDao;
+import core.basesyntax.dao.CatDaoImpl;
+import core.basesyntax.lib.Injector;
+
+/**
+ * Feel free to remove this class and create your own.
+ */
+public class Main {
+    static {
+        try {
+            Injector.injectDependency();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Если добавить котика, введите \n" +
+                "возраст котика и кго имя через пробел");
+        CatHandler.handleCat();
+        CatDao catDao = new CatDaoImpl();
+        System.out.println(catDao.getAll());
+    }
+
+}
