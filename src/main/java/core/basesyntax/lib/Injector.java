aacdd069c0a43fdf27068b017858b098629e4e33
Iurii <kenu21@gmail.com>
2019-09-05 13:12:10 +0300

    added checking types of fields.

--- src/main/java/core/basesyntax/lib/Injector.java
+++ src/main/java/core/basesyntax/lib/Injector.java
@@ -1,7 +1,9 @@
 package core.basesyntax.lib;
 
 import core.basesyntax.controller.ConsoleHandler;
+import core.basesyntax.dao.BetDao;
 import core.basesyntax.dao.BetDaoImpl;
+import core.basesyntax.dao.HumanDao;
 import core.basesyntax.dao.HumanDaoImpl;
 import core.basesyntax.factory.BetDaoFactory;
 import core.basesyntax.factory.HumanDaoFactory;
@@ -12,7 +14,8 @@ public class Injector {
     public static void injectDependency() throws IllegalAccessException {
         Field[] consoleHandlerFields = ConsoleHandler.class.getDeclaredFields();
         for (Field field : consoleHandlerFields) {
-            if (field.getDeclaredAnnotation(Inject.class) != null) {
+            if (field.getDeclaredAnnotation(Inject.class) != null
+                    && field.getType() == BetDao.class) {
                 if (BetDaoImpl.class.getDeclaredAnnotation(Dao.class) != null) {
                     field.setAccessible(true);
                     field.set(null, BetDaoFactory.getBetDao());
@@ -21,7 +24,8 @@ public class Injector {
             }
         }
         for (Field field : consoleHandlerFields) {
-            if (field.getDeclaredAnnotation(InjectHuman.class) != null) {
+            if (field.getDeclaredAnnotation(InjectHuman.class) != null
+                    && field.getType() == HumanDao.class) {
                 if (HumanDaoImpl.class.getDeclaredAnnotation(Base.class) != null) {
                     field.setAccessible(true);
                     field.set(null, HumanDaoFactory.getHumanDao());
