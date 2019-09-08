509471b078dbd8ff4b109da23bdd299eb155990c
TatianaPirch <pirch.tatiana@gmail.com>
2019-09-08 16:12:39 +0300

    annotation solution

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,28 @@
+package core.basesyntax;
+
+import core.basesyntax.controller.ConsoleHandler;
+import core.basesyntax.dao.BetDao;
+import core.basesyntax.dao.BetDaoImpl;
+import core.basesyntax.dao.UserDao;
+import core.basesyntax.dao.UserDaoImpl;
+import core.basesyntax.lib.Injector;
+
+public class Main {
+
+    static {
+        try {
+            Injector.injectDependency();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Если хотите сделать ставку, введите \n"
+                + "сумму, риск, имя и фамилию через пробел");
+        ConsoleHandler.handle();
+        BetDao betDao = new BetDaoImpl();
+        UserDao userDao = new UserDaoImpl();
+        System.out.println(betDao.getAll() + " " + userDao.getAll());
+    }
+}
---
+++ src/main/java/core/basesyntax/controller/ConsoleHandler.java
@@ -0,0 +1,41 @@
+package core.basesyntax.controller;
+
+import core.basesyntax.dao.BetDao;
+import core.basesyntax.dao.UserDao;
+import core.basesyntax.lib.Inject;
+import core.basesyntax.model.Bet;
+import core.basesyntax.model.User;
+
+import java.util.Scanner;
+
+public class ConsoleHandler {
+
+    @Inject
+    private static  BetDao betDao;
+    @Inject
+    private static UserDao userDao;
+
+    public static void handle() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                String command = scanner.nextLine();
+                if (command.equals("0")) {
+                    return;
+                }
+                try {
+                    String[] data = command.split(" ");
+                    int value = Integer.parseInt(data[0]);
+                    double risk = Double.parseDouble(data[1]);
+                    String firstName = data[2];
+                    String secondName = data[3];
+                    Bet bet = new Bet(value, risk);
+                    User user = new User(firstName, secondName);
+                    betDao.add(bet);
+                    userDao.add(user);
+                } catch (Exception e) {
+                    System.out.println("Данные введены некорректно");
+                }
+            }
+        }
+    }
+}
