8cad264df74dc9eee5a36ad16f7726b36b10b223
Max <skaterweather41@gmail.com>
2019-09-09 18:20:48 +0200

    sout removed; 1 check(field.getType) added

--- src/main/java/core/basesyntax/lib/Injector.java
+++ src/main/java/core/basesyntax/lib/Injector.java
@@ -1,6 +1,7 @@
 package core.basesyntax.lib;
 
 import core.basesyntax.controller.Controller;
+import core.basesyntax.dao.RobotDao;
 import core.basesyntax.dao.RobotDaoImpl;
 import core.basesyntax.factory.RobotDaoFactory;
 import java.lang.reflect.Field;
@@ -14,10 +15,10 @@ public class Injector {
         if (robotDaoClass.getDeclaredAnnotation(Dao.class) == null) {
             throw new IllegalArgumentException();
         }
-        System.out.println("injecting dao in field");
         Field[] controllerFields = controllerClass.getDeclaredFields();
         for (Field field : controllerFields) {
-            if (field.getDeclaredAnnotation(InjectDao.class) != null) {
+            if (field.getDeclaredAnnotation(InjectDao.class) != null
+                    && field.getType() == RobotDao.class) {
                 field.setAccessible(true);
                 field.set(null, RobotDaoFactory.getRobotDao());
             }
