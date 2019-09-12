c150674e20451f290d665ba83c02bfb157f1fe21
Taras Khalak <tarasulo@gmail.com>
2019-09-12 12:15:49 +0300

    done

--- src/main/java/core/basesyntax/dao/BetDaoImpl.java
+++ src/main/java/core/basesyntax/dao/BetDaoImpl.java
@@ -1,12 +1,10 @@
 package core.basesyntax.dao;
 
 import core.basesyntax.database.Storage;
-import core.basesyntax.lib.Dao;
 import core.basesyntax.models.Bet;
 
 import java.util.List;
 
-@Dao
 public class BetDaoImpl implements BetDao {
     @Override
     public void add(Bet bet) {
