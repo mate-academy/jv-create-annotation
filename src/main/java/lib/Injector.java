4206305f6087fbb7dc5e7e51aad94426c15a37b4
ElvenNurse <58395217+ElvenNurse@users.noreply.github.com>
2019-12-29 13:54:31 +0200

    Fixed TabletDao check in Injector, added message if there is no annotation found

--- src/main/java/lib/Injector.java
+++ src/main/java/lib/Injector.java
@@ -4,6 +4,7 @@ import controller.ConsoleHandler;
 import dao.SmartPhoneDao;
 import dao.SmartPhoneDaoImpl;
 import dao.TabletDao;
+import dao.TabletDaoImpl;
 import factory.SmartPhoneDaoFactory;
 import factory.TabletDaoFactory;
 
@@ -13,6 +14,7 @@ public class Injector {
     public static void injectDependency() throws IllegalAccessException {
         Class<ConsoleHandler> consoleHandlerClass = ConsoleHandler.class;
         Class<SmartPhoneDaoImpl> smartPhoneDaoImplClass = SmartPhoneDaoImpl.class;
+        Class<TabletDaoImpl> tabletDaoImplClass = TabletDaoImpl.class;
         Field[] consoleHandlerFields = consoleHandlerClass.getDeclaredFields();
         for (Field field : consoleHandlerFields) {
             if (field.getDeclaredAnnotation(Inject.class) != null) {
@@ -20,12 +22,16 @@ public class Injector {
                     if (smartPhoneDaoImplClass.getDeclaredAnnotation(Dao.class) != null) {
                         field.setAccessible(true);
                         field.set(null, SmartPhoneDaoFactory.getSmartPhoneDao());
+                    } else {
+                        System.out.println("Не найден SmartPhone DAO");
                     }
                 }
                 if (field.getType().equals(TabletDao.class)) {
-                    if (smartPhoneDaoImplClass.getDeclaredAnnotation(Dao.class) != null) {
+                    if (tabletDaoImplClass.getDeclaredAnnotation(Dao.class) != null) {
                         field.setAccessible(true);
                         field.set(null, TabletDaoFactory.getTabletDao());
+                    } else {
+                        System.out.println("Не найден Tablet DAO");
                     }
                 }
             }
