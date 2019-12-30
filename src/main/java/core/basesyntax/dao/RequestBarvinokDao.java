bf711d1ca4e3c1a9b0161ba65b1b480576d8c869
Mykhaylo12 <55308826+Mykhaylo12@users.noreply.github.com>
2019-12-30 18:03:12 +0200

    Update src/main/java/core/basesyntax/dao/RequestBarvinokDao.java
    Co-Authored-By: Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>

--- src/main/java/core/basesyntax/dao/RequestBarvinokDao.java
+++ src/main/java/core/basesyntax/dao/RequestBarvinokDao.java
@@ -4,7 +4,7 @@ import core.basesyntax.model.RequestBarvinok;
 
 import java.util.List;
 
-public interface RequestBarvinokDao {
+public interface BarvinokDao {
     void makeRequest(RequestBarvinok requestBarvinok);
 
     List<RequestBarvinok> getAllList();
