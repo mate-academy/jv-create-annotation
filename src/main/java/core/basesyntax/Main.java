cfb77e090096babd798fbc7f9b40f9ea406bf368
Ludchenko <skillet367@icloud.com>
2020-04-21 17:54:55 +0300

    added packages including dao, model and db

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+package core.basesyntax;
+
+import core.basesyntax.controller.ConsoleHandler;
 
 public class Main {
+    public static void main(String[] args) {
+        ConsoleHandler handler = new ConsoleHandler();
+        System.out.print("Please enter value and risk for your bet: ");
+        handler.handle();
+    }
 }
--- src/main/java/core/basesyntax/controller/ConsoleHandler.java
+++ src/main/java/core/basesyntax/controller/ConsoleHandler.java
@@ -1,4 +1,29 @@
 package core.basesyntax.controller;
 
+import core.basesyntax.model.Bet;
+import core.basesyntax.db.Storage;
+
+import java.util.Scanner;
+
 public class ConsoleHandler {
+    public void handle() {
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            String command = scanner.nextLine();
+            if(command.equalsIgnoreCase("q")) {
+                return;
+            }
+            try {
+                String[] betData = command.split(" ");
+                int value = Integer.parseInt(betData[0]);
+                double risk = Double.parseDouble(betData[1]);
+                Bet bet = new Bet(value, risk);
+                Storage.bets.add(bet);
+                System.out.println(bet.toString());
+            } catch (Exception e) {
+                System.out.println("Please enter correct values!");
+            }
+        }
+    }
 }
