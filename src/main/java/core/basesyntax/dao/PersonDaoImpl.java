398a2c0b7ebdb886ec8fc13cdbfc92cba610a0f0
vladyslav <vlad.bes0711@gmail.com>
2020-04-21 17:55:25 +0300

    added solution

---
+++ src/main/java/core/basesyntax/dao/PersonDaoImpl.java
@@ -0,0 +1,21 @@
+package core.basesyntax.dao;
+
+import core.basesyntax.lib.Dao;
+import core.basesyntax.model.Person;
+import core.basesyntax.db.Storage;
+
+import java.util.List;
+
+@Dao
+public class PersonDaoImpl implements PersonDao {
+
+    @Override
+    public void add(Person person) {
+        Storage.persons.add(person);
+    }
+
+    @Override
+    public List<Person> getAll() {
+        return Storage.persons;
+    }
+}
---
+++ src/main/java/core/basesyntax/lib/Injector.java
@@ -0,0 +1,35 @@
+package core.basesyntax.lib;
+
+import core.basesyntax.dao.BetDao;
+import core.basesyntax.factory.BetDaoFactory;
+import core.basesyntax.dao.PersonDao;
+import core.basesyntax.factory.PersonDaoFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
+public class Injector {
+    public static Object getInstance(Class clazz) throws NoSuchMethodException,
+            IllegalAccessException, InvocationTargetException, InstantiationException {
+
+        Constructor constructor = clazz.getDeclaredConstructor();
+        Object instance = constructor.newInstance();
+        Field[] declaredFields = clazz.getDeclaredFields();
+
+        for (Field field : declaredFields) {
+            if (field.getAnnotation(Inject.class) != null) {
+                field.setAccessible(true);
+                if (field.getType() == BetDao.class
+                        && BetDaoFactory.getBetDao().getClass().isAnnotationPresent(Dao.class)) {
+                    field.set(instance, BetDaoFactory.getBetDao());
+                }
+                if (field.getType() == PersonDao.class
+                        && PersonDaoFactory.getPersonDao().getClass().isAnnotationPresent(Dao.class)) {
+                    field.set(instance, PersonDaoFactory.getPersonDao());
+                }
+            }
+        }
+        return instance;
+    }
+}
