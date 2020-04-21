18024273e53c71072202027bb0d1fb930f878b98
Nick97-git <nickarchangel97@gmail.com>
2020-04-21 18:24:33 +0300

    changed access modifiers in class Storage

--- src/main/java/core/basesyntax/db/Storage.java
+++ src/main/java/core/basesyntax/db/Storage.java
@@ -6,6 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Storage {
-    public static final List<Bet> bets = new ArrayList<>();
-    public static final List<User> users = new ArrayList<>();
+    public static List<Bet> bets = new ArrayList<>();
+    public static List<User> users = new ArrayList<>();
 }
