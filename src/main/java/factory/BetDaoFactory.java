08791cb9ca2fce2045f15bce8edf6f5ae6ec3014
vladpetrovskyi <v.petrovskyi98@gmail.com>
2020-04-21 18:08:46 +0300

    updated solution for homework 13.1

--- src/main/java/factory/BetDaoFactory.java
+++ src/main/java/factory/BetDaoFactory.java
@@ -2,8 +2,6 @@ package factory;
 
 import dao.BetDao;
 import dao.BetDaoImpl;
-import dao.BetDaoJdbcImpl;
-import java.lang.annotation.Annotation;
 import lib.Dao;
 
 public class BetDaoFactory {
@@ -11,18 +9,9 @@ public class BetDaoFactory {
     private static BetDao betDao;
 
     public static BetDao getBetDao() throws NoClassDefFoundError {
-        Class<BetDaoImpl> betDaoImpl = BetDaoImpl.class;
-        Class<BetDaoJdbcImpl> betDaoJdbcImpl = BetDaoJdbcImpl.class;
-        Annotation annotation1 = betDaoImpl.getAnnotation(Dao.class);
-        Annotation annotation2 = betDaoJdbcImpl.getAnnotation(Dao.class);
-        if (annotation1 != null) {
+        if (BetDaoImpl.class.getAnnotation(Dao.class) != null) {
             if (betDao == null) {
                 betDao = new BetDaoImpl();
-            }
-            return betDao;
-        } else if (annotation2 != null) {
-            if (betDao == null) {
-                betDao = new BetDaoJdbcImpl();
             }
             return betDao;
         }
--- src/main/java/factory/PersonDaoFactory.java
+++ src/main/java/factory/PersonDaoFactory.java
@@ -2,16 +2,13 @@ package factory;
 
 import dao.PersonDao;
 import dao.PersonDaoImpl;
-import java.lang.annotation.Annotation;
 import lib.Dao;
 
 public class PersonDaoFactory {
     private static PersonDao personDao;
 
     public static PersonDao getPersonDao() throws NoClassDefFoundError {
-        Class<PersonDaoImpl> personDaoImpl = PersonDaoImpl.class;
-        Annotation annotation = personDaoImpl.getAnnotation(Dao.class);
-        if (annotation != null) {
+        if (PersonDaoImpl.class.getAnnotation(Dao.class) != null) {
             if (personDao == null) {
                 personDao = new PersonDaoImpl();
             }
