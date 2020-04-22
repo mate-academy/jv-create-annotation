c6cd9be8f59cbea3f1ea51be9dfe1e18efc9f82c
IrinaDyakonova <dyakonova.irina.v@gmail.com>
2020-04-22 10:45:24 +0300

    hw_13_with_Dao_by_PersonDaoIml

--- src/main/java/core/basesyntax/dao/PersonDaoImpl.java
+++ src/main/java/core/basesyntax/dao/PersonDaoImpl.java
@@ -1,9 +1,11 @@
 package core.basesyntax.dao;
 
 import core.basesyntax.db.Storage;
+import core.basesyntax.lib.Dao;
 import core.basesyntax.model.Person;
 import java.util.List;
 
+@Dao
 public class PersonDaoImpl implements PersonDao {
     @Override
     public void add(Person person) {
