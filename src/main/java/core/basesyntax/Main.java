70fdee2f1189a66ab840ad7d6d408a62d17bcbec
andriy <maziarandriy@gmail.com>
2020-04-22 11:26:23 +0300

    added own exception and added more logic in injector class

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,12 +1,14 @@
 package core.basesyntax;
 
 import core.basesyntax.controller.ConsoleHandler;
+import core.basesyntax.exceptions.NoSuchDaoImplementation;
 import core.basesyntax.library.Injector;
 import java.lang.reflect.InvocationTargetException;
 
 public class Main {
     public static void main(String[] args) throws InvocationTargetException,
-            NoSuchMethodException, InstantiationException, IllegalAccessException {
+            NoSuchMethodException, InstantiationException,
+            IllegalAccessException, NoSuchDaoImplementation {
         ConsoleHandler handler = (ConsoleHandler) Injector.getInstance(ConsoleHandler.class);
         System.out.println("Enter your bet Value, risk, First Name and Last Name: ");
         handler.handle();
--- src/main/java/core/basesyntax/library/Injector.java
+++ src/main/java/core/basesyntax/library/Injector.java
@@ -2,6 +2,7 @@ package core.basesyntax.library;
 
 import core.basesyntax.dao.BetDao;
 import core.basesyntax.dao.UserDao;
+import core.basesyntax.exceptions.NoSuchDaoImplementation;
 import core.basesyntax.factory.Factory;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
@@ -9,23 +10,26 @@ import java.lang.reflect.InvocationTargetException;
 
 public class Injector {
     public static Object getInstance(Class clazz) throws NoSuchMethodException,
-            IllegalAccessException, InvocationTargetException, InstantiationException {
+            IllegalAccessException, InvocationTargetException,
+            InstantiationException, NoSuchDaoImplementation {
         Constructor constructor = clazz.getDeclaredConstructor();
         Object instance = constructor.newInstance();
+        Object init;
 
         Field[] declaredFields = clazz.getDeclaredFields();
         for (Field field : declaredFields) {
-            if (field.getAnnotation(Inject.class) != null
-                    && field.getType() == BetDao.class
-                    && field.getAnnotation(Dao.class) != null) {
+            if (field.getAnnotation(Inject.class) != null) {
                 field.setAccessible(true);
-                field.set(instance, Factory.getBetDao());
-            }
-            if (field.getAnnotation(Inject.class) != null
-                    && field.getType() == UserDao.class
-                    && field.getAnnotation(Dao.class) != null) {
-                field.setAccessible(true);
-                field.set(instance, Factory.getUserDao());
+                if (field.getType() == BetDao.class
+                        && Factory.getBetDao().getClass().getAnnotation(Dao.class) != null) {
+                    init = Factory.getBetDao();
+                } else if (field.getType() == UserDao.class
+                        && Factory.getUserDao().getClass().getAnnotation(Dao.class) != null) {
+                    init = Factory.getUserDao();
+                } else {
+                    throw new NoSuchDaoImplementation("No such dao Implementation");
+                }
+                field.set(instance, init);
             }
         }
         return instance;
