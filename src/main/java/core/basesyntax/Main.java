62c9597d8e26e89a6e857a3f73a32568f5a57a9e
Ludchenko <skillet367@icloud.com>
2020-04-22 16:49:03 +0300

    added full solution

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,11 +1,25 @@
 package core.basesyntax;
 
 import core.basesyntax.controller.ConsoleHandler;
+import core.basesyntax.dao.BetDao;
+import core.basesyntax.dao.UserDao;
+import core.basesyntax.exceptions.NoSuchImplementationException;
+import core.basesyntax.factory.Factory;
+import core.basesyntax.lib.Injector;
+import java.lang.reflect.InvocationTargetException;
 
 public class Main {
-    public static void main(String[] args) {
-        ConsoleHandler handler = new ConsoleHandler();
-        System.out.print("Please enter value and risk for your bet: ");
+    public static void main(String[] args) throws InvocationTargetException,
+            NoSuchMethodException, InstantiationException,
+            IllegalAccessException, NoSuchImplementationException {
+        ConsoleHandler handler = (ConsoleHandler)Injector
+                .getInstance(ConsoleHandler.class);
+        System.out.print("Please enter value, risk for your bet and your age: ");
         handler.handle();
+
+        BetDao betDao = Factory.getBetDao();
+        UserDao userDao = Factory.getUserDao();
+        System.out.println("All bets: " + betDao.getAll());
+        System.out.println("All ages: " + userDao.getAll());
     }
 }
--- src/main/java/core/basesyntax/controller/ConsoleHandler.java
+++ src/main/java/core/basesyntax/controller/ConsoleHandler.java
@@ -1,29 +1,44 @@
 package core.basesyntax.controller;
 
+import core.basesyntax.dao.BetDao;
+import core.basesyntax.dao.UserDao;
+import core.basesyntax.lib.Inject;
 import core.basesyntax.model.Bet;
-import core.basesyntax.db.Storage;
-
+import core.basesyntax.model.User;
 import java.util.Scanner;
 
 public class ConsoleHandler {
+    @Inject
+    BetDao betDao;
+
+    @Inject
+    UserDao userDao;
+
     public void handle() {
         Scanner scanner = new Scanner(System.in);
 
-        while(true) {
+        while (true) {
             String command = scanner.nextLine();
-            if(command.equalsIgnoreCase("q")) {
+            if (command.equalsIgnoreCase("q")) {
                 return;
             }
+            Bet bet = null;
+            User user = null;
             try {
                 String[] betData = command.split(" ");
                 int value = Integer.parseInt(betData[0]);
                 double risk = Double.parseDouble(betData[1]);
-                Bet bet = new Bet(value, risk);
-                Storage.bets.add(bet);
-                System.out.println(bet.toString());
-            } catch (Exception e) {
+                int age = Integer.parseInt(betData[2]);
+                bet = new Bet(value, risk);
+                user = new User(age);
+            } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
                 System.out.println("Please enter correct values!");
             }
+            betDao.add(bet);
+            userDao.add(user);
+
+            System.out.println(bet == null ? null : bet.toString());
+            System.out.println(user == null ? null : user.toString());
         }
     }
 }
