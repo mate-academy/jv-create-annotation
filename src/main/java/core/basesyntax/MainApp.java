911627ea9434c6f4f07b0056d38a637cabbccd64
xeddin <61269748+xeddin@users.noreply.github.com>
2020-04-22 13:52:27 +0300

    Added exception, some refactoring in Injector

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,12 +1,13 @@
 package core.basesyntax;
 
 import core.basesyntax.controller.ConsoleHandler;
+import core.basesyntax.exceptions.DaoException;
 import core.basesyntax.lib.Injector;
 import java.lang.reflect.InvocationTargetException;
 
 public class MainApp {
     public static void main(String[] args) throws InvocationTargetException,
-            NoSuchMethodException, InstantiationException, IllegalAccessException {
+            NoSuchMethodException, InstantiationException, IllegalAccessException, DaoException {
         ConsoleHandler handler = (ConsoleHandler) Injector.getInstance(ConsoleHandler.class);
         System.out.println("Enter Name, age, value and risk for your bet or Q for exit:");
         handler.handle();
