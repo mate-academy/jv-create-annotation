020fe7a1db99a732ecc4bb9e1fb2fb72f0e51d5a
Polina Kulyk <polina.kulyk@protonmail.com>
2020-04-22 07:36:49 +0300

    Added exception and check for Dao presence

---
+++ src/main/java/core/basesyntax/exception/AbsentDaoAnnotatedFieldException.java
@@ -0,0 +1,7 @@
+package core.basesyntax.exception;
+
+public class AbsentDaoAnnotatedFieldException extends Exception {
+    public AbsentDaoAnnotatedFieldException(String message) {
+        super(message);
+    }
+}
--- src/main/java/core/basesyntax/lib/Injector.java
+++ src/main/java/core/basesyntax/lib/Injector.java
@@ -4,6 +4,7 @@ import core.basesyntax.dao.BetDao;
 import core.basesyntax.dao.BetDaoImpl;
 import core.basesyntax.dao.UserDao;
 import core.basesyntax.dao.UserDaoImpl;
+import core.basesyntax.exception.AbsentDaoAnnotatedFieldException;
 import core.basesyntax.factory.BetDaoFactory;
 import core.basesyntax.factory.UserDaoFactory;
 import java.lang.reflect.Constructor;
@@ -12,12 +13,18 @@ import java.lang.reflect.InvocationTargetException;
 
 public class Injector {
     public static Object getInstance(Class clazz) throws NoSuchMethodException,
-            IllegalAccessException, InvocationTargetException, InstantiationException {
+            IllegalAccessException, InvocationTargetException, InstantiationException,
+            AbsentDaoAnnotatedFieldException {
         Constructor constructor = clazz.getDeclaredConstructor();
         Object instance = constructor.newInstance();
         Class<UserDaoImpl> userDaoImplClass = UserDaoImpl.class;
         Class<BetDaoImpl> betDaoImplClass = BetDaoImpl.class;
         Field[] fields = clazz.getDeclaredFields();
+
+        if (clazz.getAnnotation(Dao.class) == null) {
+            throw new AbsentDaoAnnotatedFieldException("Dao not found for this class");
+        }
+
         for (Field field : fields) {
             if (field.getAnnotation(Inject.class) != null) {
                 if (userDaoImplClass.getAnnotation(Dao.class) != null
