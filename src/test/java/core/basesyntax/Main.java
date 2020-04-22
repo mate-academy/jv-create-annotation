7e2dfa5bcab54394bd8653585fcd565106709832
Djeckkenni <djeck27kenni@gmail.com>
2020-04-22 11:27:24 +0300

    added solution

---
+++ src/test/java/core/basesyntax/Main.java
@@ -0,0 +1,18 @@
+package core.basesyntax;
+
+import core.basesyntax.controller.ConsoleHandler;
+import core.basesyntax.dao.BetDao;
+import core.basesyntax.dao.UserDao;
+import core.basesyntax.factory.Factory;
+import core.basesyntax.lib.Injector;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+    public static void main(String[] args) throws InvocationTargetException,
+            NoSuchMethodException, InstantiationException, IllegalAccessException {
+        //ConsoleHandler handler = new ConsoleHandler();
+        ConsoleHandler handler = (ConsoleHandler) Injector.getInstance(ConsoleHandler.class);
+        handler.handle();
+    }
+}
