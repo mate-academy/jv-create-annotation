c91f5b976f6471dcdcb1e730aa1d84f584023629
Serhii Tsyhankov <40259977+fruts@users.noreply.github.com>
2020-04-22 11:08:51 +0300

    edit code style mistakes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,12 @@
 import controller.ConsoleHandler;
-import java.lang.reflect.InvocationTargetException;
-
 import exception.NoDaoImplementException;
+import java.lang.reflect.InvocationTargetException;
 import lib.Injector;
 
 public class Main {
     public static void main(String[] args) throws InvocationTargetException,
-            NoSuchMethodException, InstantiationException, IllegalAccessException, NoDaoImplementException {
+            NoSuchMethodException, InstantiationException,
+            IllegalAccessException, NoDaoImplementException {
         ConsoleHandler handler = (ConsoleHandler) Injector.getInstance(ConsoleHandler.class);
         System.out.println("Add users. Like: email accNumber");
         handler.createUser();
