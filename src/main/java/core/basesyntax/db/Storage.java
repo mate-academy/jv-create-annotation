6b74c46a319e493dd7df12a799d3a0b3ddc24e9a
Galina <gala6ap@mail.ru>
2020-04-22 14:54:22 +0300

    refactoring

--- src/main/java/core/basesyntax/db/Storage.java
+++ src/main/java/core/basesyntax/db/Storage.java
@@ -1,10 +1,9 @@
 package core.basesyntax.db;
 
-import java.util.ArrayList;
-import java.util.List;
 import core.basesyntax.model.Bet;
 import core.basesyntax.model.User;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Storage {
     public static List<Bet> bets = new ArrayList<>();
