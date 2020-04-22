345e2848ac3ab0d5e0521028d62d665356cb1137
VovkAA <vovkandrew@gmail.com>
2020-04-22 13:42:46 +0300

    Done

---
+++ src/main/java/core/basesyntax/dao/BetDaoJdbcImpl.java
@@ -0,0 +1,17 @@
+package core.basesyntax.dao;
+
+import core.basesyntax.database.Storage;
+import core.basesyntax.models.Bet;
+import java.util.List;
+
+public class BetDaoJdbcImpl implements BetDao {
+    @Override
+    public void add(Bet bet) {
+        Storage.bets.add(bet);
+    }
+
+    @Override
+    public List<Bet> getAll() {
+        return Storage.bets;
+    }
+}
---
+++ src/main/java/core/basesyntax/library/Injector.java
@@ -0,0 +1,34 @@
+package core.basesyntax.library;
+
+import core.basesyntax.dao.BetDao;
+import core.basesyntax.dao.BetDaoImp;
+import core.basesyntax.dao.UserDao;
+import core.basesyntax.factory.Factory;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
+public class Injector {
+
+    public static Object getInstance(Class clazz) throws NoSuchMethodException,
+            IllegalAccessException, InvocationTargetException, InstantiationException {
+        Constructor constructor = clazz.getDeclaredConstructor();
+        Object instance = constructor.newInstance();
+        Field[] clazzFields = clazz.getDeclaredFields();
+        for (Field field: clazzFields) {
+            if (field.getAnnotation(Inject.class) != null) {
+                if (field.getType().equals(BetDao.class)
+                        && BetDaoImp.class.getAnnotation(Dao.class) != null) {
+                    field.setAccessible(true);
+                    field.set(instance, Factory.getBetDao());
+                }
+                if (field.getType().equals(UserDao.class)
+                        && BetDaoImp.class.getAnnotation(Dao.class) != null) {
+                    field.setAccessible(true);
+                    field.set(instance, Factory.getUserDao());
+                }
+            }
+        }
+        return instance;
+    }
+}
