5d3349ccfd574b8d60c867f8d2cb2e05f1d29aca
Богдан Демьянчук <t0974447144@gmail.com>
2020-04-22 15:00:43 +0300

    Add Exception

--- src/main/java/lib/Injector.java
+++ src/main/java/lib/Injector.java
@@ -22,13 +22,15 @@ public class Injector {
                     if (betDao.getClass().isAnnotationPresent(Dao.class)) {
                         field.set(instance, betDao);
                     } else {
-                        throw new NoSuchDaoException("This class not have abotation Dao");
+                        throw new NoSuchDaoException("This class not have anotation Dao");
                     }
                 } else if (field.getType() == PersonDao.class) {
                     field.setAccessible(true);
                     PersonDao personDao = Factory.getPersonDao();
                     if (personDao.getClass().isAnnotationPresent(Dao.class)) {
                         field.set(instance, personDao);
+                    } else {
+                        throw new NoSuchDaoException("This class not have anotation Dao");
                     }
                 }
             }
