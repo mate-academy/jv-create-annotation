4e180eae27c652dfa3fba1761ab81988fb7f30ff
Pavlo Khomiak <pavlo.khomiak93@gmail.com>
2020-08-31 15:55:45 +0300

    Add solution

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -5,7 +5,6 @@ import core.basesyntax.controller.ConsoleHandler;
 public class Main {
     public static void main(String[] args) {
         ConsoleHandler consoleHandler = new ConsoleHandler();
-        //System.out.println("Input value and risk for your bet");
         consoleHandler.handle();
     }
 }
