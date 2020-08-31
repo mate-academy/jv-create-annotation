e3ee6de71ccb5bc983bc76ff9dbfaa74a53e6074
Marina <maryna.voloshyna.11@gmail.com>
2020-08-31 19:58:46 +0300

    sol

--- src/main/java/core/basesyntax/UserStorage.java
+++ src/main/java/core/basesyntax/UserStorage.java
@@ -1,9 +1,7 @@
 package core.basesyntax;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class UserStorage {
     private List<User> userList = new ArrayList<>();
