06d9de9152d20452bf1e27e42a0beedbabcc7dfc
Leonid <rommel@ukr.net>
2020-08-31 10:46:02 +0300

    User class created

---
+++ src/main/java/core/basesyntax/controller/ConsoleHandler.java
@@ -0,0 +1,39 @@
+package core.basesyntax.controller;
+
+import core.basesyntax.dao.BetDao;
+import core.basesyntax.dao.UserDao;
+import core.basesyntax.lib.Inject;
+import core.basesyntax.model.Bet;
+import core.basesyntax.model.User;
+import java.util.Scanner;
+
+public class ConsoleHandler {
+    @Inject
+    private BetDao betDao;
+
+    @Inject
+    private UserDao userDao;
+
+    public void handle() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equalsIgnoreCase("q")) {
+                return;
+            }
+
+            Bet bet = null;
+            try {
+                String[] betData = command.split(" ");
+                int value = Integer.parseInt(betData[0]);
+                double risk = Double.parseDouble(betData[1]);
+                bet = new Bet(value, risk);
+            } catch (NumberFormatException e) {
+                System.out.println("Enter correct data");
+            }
+            betDao.add(bet);
+            System.out.println(bet);
+        }
+    }
+}
