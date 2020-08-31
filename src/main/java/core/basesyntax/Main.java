eb1ccc5719dcb52455a0271f018fb7fd760fbcf6
vosker <nikita.vaganov16@gmail.com>
2020-08-31 22:34:48 +0300

    testing commits

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -8,7 +8,7 @@ import core.basesyntax.controller.ConsoleHandler;
 public class Main {
     public static void main(String[] args) {
         ConsoleHandler handler = new ConsoleHandler();
-        System.out.println("Please enter your bet amount and its risk. If you want stop, enter Q.");
+        System.out.println("Please enter your bet amount and its risk. If you wantек stop, enter Q.");
         handler.handle();
     }
 }
