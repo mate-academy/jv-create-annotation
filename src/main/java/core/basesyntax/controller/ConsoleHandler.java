2ccd7430d2292a5319535364e77c9338e4f5e503
AnaLanda <56306772+AnaLanda@users.noreply.github.com>
2020-08-31 11:47:31 +0300

    created User class & BetDao and UserDao interfaces with tehir implementations

--- src/main/java/core/basesyntax/controller/ConsoleHandler.java
+++ src/main/java/core/basesyntax/controller/ConsoleHandler.java
@@ -1,9 +1,12 @@
 package core.basesyntax.controller;
 
+import core.basesyntax.dao.BetDao;
+import core.basesyntax.dao.BetDaoImpl;
 import core.basesyntax.model.Bet;
 import java.util.Scanner;
 
 public class ConsoleHandler {
+    BetDao betDao = new BetDaoImpl();
 
     public void handle() {
         Scanner scanner = new Scanner(System.in);
@@ -12,15 +15,17 @@ public class ConsoleHandler {
             if (command.equalsIgnoreCase("q")) {
                 return;
             }
+            Bet bet = null;
             try {
                 String[] betData = command.split(" ");
                 int amount = Integer.parseInt(betData[0]);
                 double risk = Double.parseDouble(betData[1]);
-                Bet bet = new Bet(amount, risk);
-                System.out.println(bet.toString());
+                bet = new Bet(amount, risk);
             } catch (NumberFormatException e) {
                 System.out.println("Please enter numbers in an appropriate format.");
             }
+            betDao.add(bet);
+            System.out.println(bet);
         }
     }
 }
