8c025390264a4cb5305af1e45ce230b96c0f9c63
vosker <nikita.vaganov16@gmail.com>
2020-08-31 22:18:46 +0300

    solution added

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,14 @@
+package core.basesyntax;
+
+import core.basesyntax.controller.ConsoleHandler;
+
+/**
+ * Feel free to remove this class and create your own.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ConsoleHandler handler = new ConsoleHandler();
+        System.out.println("Please enter your bet amount and its risk. If you want stop, enter Q.");
+        handler.handle();
+    }
+}
