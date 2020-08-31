17ef6075ec10d20fe79dcf94fe4d923efc2b0d9c
Ilya.Kvitkovskyi <ilya.kvitkovskyi@gmail.com>
2020-08-31 17:56:57 +0300

    create User, UserDao and UserDao implementation

---
+++ src/main/java/core/basesyntax/controller/ConsoleHandler.java
@@ -0,0 +1,37 @@
+package core.basesyntax.controller;
+
+import core.basesyntax.dao.BetDao;
+import core.basesyntax.dao.UserDao;
+import core.basesyntax.model.Bet;
+import core.basesyntax.model.User;
+import java.util.Scanner;
+
+public class ConsoleHandler {
+    private BetDao betDao;
+    private UserDao userDao;
+
+    public void handle() {
+        Scanner scanner = new Scanner(System.in);
+        Bet bet = null;
+        User user = null;
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("q")) {
+                break;
+            }
+            try {
+                String[] betAndUserData = input.split(" ");
+                int value = Integer.parseInt(betAndUserData[0]);
+                double risk = Double.parseDouble(betAndUserData[1]);
+                bet = new Bet(value, risk);
+                String name = betAndUserData[2];
+                int age = Integer.parseInt(betAndUserData[3]);
+                user = new User(name, age);
+            } catch (NumberFormatException e) {
+                System.out.println("Enter correct information, please!" + e);
+            }
+            betDao.add(bet);
+            userDao.add(user);
+        }
+    }
+}
---
+++ src/main/java/core/basesyntax/model/Bet.java
@@ -0,0 +1,15 @@
+package core.basesyntax.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Bet {
+    private int value;
+    private double risk;
+}
