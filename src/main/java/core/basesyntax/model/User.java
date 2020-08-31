1fc1ed3614e46b4fe93043bae819cc275ad7a56f
Ilya <ilya03449@gmail.com>
2020-08-31 15:30:01 +0300

    A bit changed class "User"

--- src/main/java/core/basesyntax/model/User.java
+++ src/main/java/core/basesyntax/model/User.java
@@ -4,18 +4,12 @@ public class User {
     private String name;
     private String surname;
     private int age;
+    private String password;
+    private final String login;
 
-    public User(String name, String surname, int age) {
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "User {name = '" + name
-                + '\'' + ", surname = '" + surname
-                + '\'' + ", age = " + age + '}';
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
     }
 
     public String getName() {
@@ -40,5 +34,17 @@ public class User {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
