3d2a83e345591caba0a662d6d1d1f8c9c0593bbc
Yutsevich Anastasia <yutsevicha@ gmail.com>
2020-08-31 16:30:38 +0300

    first part done

---
+++ src/main/java/core/basesyntax/controller/ConsoleHandler.java
@@ -0,0 +1,33 @@
+package core.basesyntax.controller;
+
+import core.basesyntax.dao.BetDaoImpl;
+import core.basesyntax.dao.UserDaoImpl;
+import core.basesyntax.dao.interfaces.BetDao;
+import core.basesyntax.dao.interfaces.UserDao;
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.Bet;
+import java.util.Scanner;
+
+public class ConsoleHandler {
+    private BetDao betDao = new BetDaoImpl();
+    private UserDao userDao = new UserDaoImpl();
+
+    public void handle() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("q")) {
+                break;
+            }
+            try {
+                betDao.add(new Bet(Integer.parseInt(input.split(" ")[0]),
+                        Double.parseDouble(input.split(" ")[1])));
+            } catch (NumberFormatException exception) {
+                System.out.println("Please, enter valid data");
+            }
+        }
+
+        System.out.println(Storage.bets.toString());
+    }
+}
