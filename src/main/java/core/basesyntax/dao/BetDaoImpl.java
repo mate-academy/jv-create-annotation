b974b801c78e244c3b7cfd92a27af02a5c1ac773
Laszlo Petechuk <frostpv@gmail.com>
2020-09-01 15:33:52 +0300

    Added check for @dao

--- src/main/java/core/basesyntax/dao/BetDaoImpl.java
+++ src/main/java/core/basesyntax/dao/BetDaoImpl.java
@@ -1,9 +1,11 @@
 package core.basesyntax.dao;
 
 import core.basesyntax.db.Storage;
+import core.basesyntax.lib.Dao;
 import core.basesyntax.models.BetModel;
 import java.util.List;
 
+@Dao
 public class BetDaoImpl implements BetDao {
     @Override
     public void add(BetModel bet) {
--- src/main/java/core/basesyntax/lib/Injector.java
+++ src/main/java/core/basesyntax/lib/Injector.java
@@ -1,9 +1,11 @@
 package core.basesyntax.lib;
 
 import core.basesyntax.dao.BetDao;
+import core.basesyntax.dao.BetDaoImpl;
 import core.basesyntax.dao.UserDao;
 import core.basesyntax.factory.Factory;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -18,14 +20,24 @@ public class Injector {
         for (Field field : declaredFields) {
             if (field.getAnnotation(Inject.class) != null) {
                 field.setAccessible(true);
-                if (BetDao.class.equals(field.getType())) {
+                if (BetDao.class.equals(field.getType())
+                && isDao(Factory.getBetDao().getClass())) {
                     field.set(instance, Factory.getBetDao());
                 }
-                if (UserDao.class.equals(field.getType())) {
+                if (UserDao.class.equals(field.getType())
+                && isDao(Factory.getUserDao().getClass())) {
                     field.set(instance, Factory.getUserDao());
                 }
             }
         }
         return instance;
+    }
+
+    private static boolean isDao(Class clazz) {
+        Annotation[] annotations = clazz.getAnnotations();
+        for (Annotation annotation: annotations) {
+           return annotation.annotationType().getCanonicalName().equals(Dao.class.getName());
+        }
+        throw new RuntimeException("DaoIsUnusuable");
     }
 }
