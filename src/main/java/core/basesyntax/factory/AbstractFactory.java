beae89614588849e53fc740ca83045fb6898e5e3
romanHreits <romagreic@gmail.com>
2020-09-01 17:27:25 +0300

    added custom exception, added Target annotation in Dao annotation

--- src/main/java/core/basesyntax/factory/AbstractFactory.java
+++ src/main/java/core/basesyntax/factory/AbstractFactory.java
@@ -2,9 +2,9 @@ package core.basesyntax.factory;
 
 import core.basesyntax.controller.BetConsoleHandler;
 import core.basesyntax.controller.UserConsoleHandler;
+import core.basesyntax.exception.NoSuchInjectionObjectException;
 import core.basesyntax.lib.Dao;
 import java.util.Map;
-import java.util.NoSuchElementException;
 
 public class AbstractFactory {
     private static final Map<Class, Object> OBJ_FACTORIES = Map
@@ -14,7 +14,7 @@ public class AbstractFactory {
     public static Object getNecessaryInstance(Class clazz) {
         Object obj = OBJ_FACTORIES.get(clazz);
         if (obj == null || !(obj.getClass().isAnnotationPresent(Dao.class))) {
-            throw new NoSuchElementException("The required injection object was not found");
+            throw new NoSuchInjectionObjectException("The required injection object was not found");
         }
         return obj;
     }
