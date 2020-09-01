b6f8e153e9ee8469286698a1d3764e8c4935ac50
Laszlo Petechuk <frostpv@gmail.com>
2020-09-01 15:47:48 +0300

    Fixed main

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -12,6 +12,6 @@ public class Main {
         System.out.println("Please input data");
         controllerHandler.handleBet();
         System.out.println("Please input user");
-        controllerHandler.handleBet();
+        controllerHandler.handleUser();
     }
 }
