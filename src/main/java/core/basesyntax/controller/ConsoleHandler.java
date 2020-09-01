6c24375a38ebc3bb4b5451cc50aba247298377e0
Artem Vremere <vremere_a@ukr.net>
2020-09-01 12:22:56 +0300

    added MyException

--- src/main/java/core/basesyntax/controller/ConsoleHandler.java
+++ src/main/java/core/basesyntax/controller/ConsoleHandler.java
@@ -5,6 +5,7 @@ import core.basesyntax.dao.OrderDao;
 import core.basesyntax.lib.Inject;
 import core.basesyntax.model.Bet;
 import core.basesyntax.model.Order;
+
 import java.util.Scanner;
 
 public class ConsoleHandler {
@@ -21,41 +22,39 @@ public class ConsoleHandler {
             System.out.println("Please, enter value and risk, for your bet");
             while (true) {
                 command = scanner.nextLine();
-//            String command = scanner.nextLine();
                 if (command.equalsIgnoreCase("q")) {
                     return;
                 }
-                Bet bet = null;
+                Bet bet;
                 try {
                     String[] betData = command.split(" ");
                     int value = Integer.parseInt(betData[0]);
                     double risk = Double.parseDouble(betData[1]);
                     bet = new Bet(value, risk);
-                } catch (NumberFormatException e) {
+                    betDao.add(bet);
+                    System.out.println(bet == null ? null : bet.toString());
+                } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
                     System.out.println("Please, enter correct data");
                 }
-                betDao.add(bet);
-                System.out.println(bet == null ? null : bet.toString());
             }
         } else if (command.equalsIgnoreCase("order")) {
             System.out.println("Please, enter name of product and quantity, for your order");
             while (true) {
                 command = scanner.nextLine();
-//            String command = scanner.nextLine();
                 if (command.equalsIgnoreCase("q")) {
                     return;
                 }
-                Order order = null;
+                Order order;
                 try {
                     String[] orderData = command.split(" ");
                     String nameOfProduct = orderData[0];
                     int quantity = Integer.parseInt(orderData[1]);
                     order = new Order(nameOfProduct, quantity);
-                } catch (NumberFormatException e) {
+                    orderDao.add(order);
+                    System.out.println(order == null ? null : order.toString());
+                } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
                     System.out.println("Please, enter correct data");
                 }
-                orderDao.add(order);
-                System.out.println(order == null ? null : order.toString());
             }
         }
     }
---
+++ src/main/java/core/basesyntax/exception/MyException.java
@@ -0,0 +1,7 @@
+package core.basesyntax.exception;
+
+public class MyException extends Exception {
+    public MyException(String message) {
+        super(message);
+    }
+}
