c59a2c348b4516faf756aa01a6ee1923c39c415f
tarkaiev <tarkaievd@gmail.com>
2020-09-01 16:33:34 +0300

    added injection

--- src/main/java/core/basesyntax/controller/ConsoleHandler.java
+++ src/main/java/core/basesyntax/controller/ConsoleHandler.java
@@ -1,27 +1,29 @@
 package core.basesyntax.controller;
 
 import core.basesyntax.dao.BetDao;
-import core.basesyntax.dao.BetDaoImpl;
 import core.basesyntax.dao.UserDao;
-import core.basesyntax.dao.UserDaoImpl;
+import core.basesyntax.lib.Inject;
 import core.basesyntax.model.Bet;
 import core.basesyntax.model.User;
 import java.util.Scanner;
 
 public class ConsoleHandler {
-    BetDao betDao = new BetDaoImpl();
-    UserDao userDao = new UserDaoImpl();
+    @Inject
+    private BetDao betDao;
+
+    @Inject
+    private UserDao userDao;
 
     public void handle() {
         Scanner scanner = new Scanner(System.in);
+        User user;
+        Bet bet;
 
         while (true) {
             String input = scanner.nextLine();
             if (input.equalsIgnoreCase("q")) {
                 return;
             }
-            User user = null;
-            Bet bet = null;
 
             try {
                 String[] betAndUserData = input.split(" ");
@@ -35,15 +37,16 @@ public class ConsoleHandler {
                 int value = Integer.parseInt(betAndUserData[2]);
                 double risk = Double.parseDouble(betAndUserData[3]);
                 bet = new Bet(value, risk);
+
+                userDao.addUser(user);
+                betDao.addBet(bet);
+
+                System.out.println(userDao.getAllUsers());
+                System.out.println(betDao.getAllBets());
             } catch (NumberFormatException | IndexOutOfBoundsException e) {
                 System.out.println("Input data is incorrect");
             }
 
-            userDao.addUser(user);
-            betDao.addBet(bet);
-
-            System.out.println(userDao.getAllUsers());
-            System.out.println(betDao.getAllBets());
         }
     }
 }
