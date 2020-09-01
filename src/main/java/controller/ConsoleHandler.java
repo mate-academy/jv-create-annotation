aefbb3d1a37bb6b67b1d328af800a4f9ae1172c9
m.mikhajlov <lizappy31@gmail.com>
2020-09-01 13:59:46 +0300

    create BetService, divided handle method to few methods, change storage from Map to ArrayList

--- src/main/java/controller/ConsoleHandler.java
+++ src/main/java/controller/ConsoleHandler.java
@@ -1,22 +1,26 @@
 package controller;
 
-import dao.BetDao;
-import dao.BetDaoImpl;
-import dao.UserDaoImpl;
 import java.util.Scanner;
+
 import model.Bet;
 import model.User;
+import services.BetService;
 import services.UserService;
 
 public class ConsoleHandler {
     private static final int DATA_QUANTITY = 2;
-    BetDao betDao = new BetDaoImpl();
+    private static final int LOGIN_PASSWORD_MIN_LENGTH = 3;
+
+    private UserService userService = new UserService();
+    private BetService betService = new BetService();
 
     public void handle() {
         Scanner scanner = new Scanner(System.in);
-        UserDaoImpl userDao = new UserDaoImpl();
-        User user = null;
+        User user = enterLoginPassword(scanner);
+        enterValueAndRisk(scanner, user);
+    }
 
+    private User enterLoginPassword(Scanner scanner) {
         System.out.println("Введите логин и пароль:");
         String creds = scanner.nextLine();
         String[] userData = creds.split(" ");
@@ -25,44 +29,51 @@ public class ConsoleHandler {
         }
         String login = userData[0];
         String password = userData[1];
-        if (checkCreds(login, password)) {
-            user = userDao.login(login, password);
-        } else {
+        if (!checkCreds(login, password)) {
             handle();
         }
+        return userService.getUserDao().login(login, password);
+    }
+
+    private void enterValueAndRisk(Scanner scanner, User user) {
+        Bet bet = null;
         System.out.println("Введите value и risk:");
         while (true) {
             String command = scanner.nextLine();
-            if (command.equalsIgnoreCase("q")) {
+            if (isCommandTerminate(command, user)) {
                 break;
             }
-            if (command.equalsIgnoreCase("pr")) {
-                UserService userService = new UserService();
-                userService.printUsersBetList(user.getUserName());
-                break;
-            }
-            Bet bet = null;
             try {
                 String[] betData = command.split(" ");
                 int value = Integer.parseInt(betData[0]);
                 double risk = Double.parseDouble(betData[1]);
                 bet = new Bet(value, risk, user);
                 user.getBetsList().add(bet);
-
-            } catch (NumberFormatException e) {
+            } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
                 System.out.println("Please, enter correct data");
             }
-            betDao.add(bet);
-            System.out.println(bet == null ? null : bet.toString());
+            betService.getBetDao().add(bet);
         }
     }
 
     private boolean checkCreds(String login, String password) {
         if (login != null && password != null
-                && login.length() > 3 && password.length() > 3) {
+                && login.length() > LOGIN_PASSWORD_MIN_LENGTH
+                && password.length() > LOGIN_PASSWORD_MIN_LENGTH) {
             return true;
         }
         System.out.println("Login and password can't be empty or shorter 3 symbols!");
+        return false;
+    }
+
+    private boolean isCommandTerminate(String command, User user) {
+        if (command.equalsIgnoreCase("q")) {
+            return true;
+        }
+        if (command.equalsIgnoreCase("print")) {
+            userService.printUsersBetList(user);
+            return true;
+        }
         return false;
     }
 }
--- src/main/java/db/Storage.java
+++ src/main/java/db/Storage.java
@@ -3,7 +3,9 @@ package db;
 import java.util.ArrayList;
 import java.util.List;
 import model.Bet;
+import model.User;
 
-public class BetStorage {
+public class Storage {
+    public static final List<User> userStorage = new ArrayList<>();
     public static final List<Bet> bets = new ArrayList<>();
 }
