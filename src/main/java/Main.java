10f2df8a947f57042551c694de7435d9cddcf696
Roman <matviichuk.office@gmail.com>
2020-09-01 12:05:28 +0300

    added exceptions and user to handle() method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 import core.basesyntax.controller.ConsoleHandler;
 
 public class Main {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         ConsoleHandler handler = new ConsoleHandler();
-        System.out.println("Please, enter value and risk for your bet!");
+        System.out.println("Enter yours login, password, value, risk. Push ENTER! "
+                + "After should write GO");
         handler.handle();
+
     }
 }
--- src/main/java/core/basesyntax/controller/ConsoleHandler.java
+++ src/main/java/core/basesyntax/controller/ConsoleHandler.java
@@ -2,30 +2,44 @@ package core.basesyntax.controller;
 
 import core.basesyntax.dao.BetDao;
 import core.basesyntax.dao.BetDaoImp;
+import core.basesyntax.dao.UserDao;
+import core.basesyntax.dao.UserDaoImp;
 import core.basesyntax.model.Bet;
+import core.basesyntax.model.User;
 import java.util.Scanner;
 
 public class ConsoleHandler {
     public void handle() {
         Scanner scanner = new Scanner(System.in);
         BetDao betDao = new BetDaoImp();
+        UserDao userDao = new UserDaoImp();
+
         while (true) {
             String command = scanner.nextLine();
-            if (command.equalsIgnoreCase("q")) {
+            if (command.equalsIgnoreCase("go")) {
+                System.out.println("break a leg!");
                 return;
             }
+            User user = null;
             Bet bet = null;
             try {
                 String[] betData = command.split(" ");
-                int value = Integer.parseInt(betData[0]);
-                double risk = Double.parseDouble(betData[1]);
-                bet = new Bet(value, risk);
-
-            } catch (NumberFormatException e) {
-                System.out.println("Please, enter correct data!");
+                if (betData.length == 4) {
+                    String login = betData[0];
+                    String password = betData[1];
+                    int value = Integer.parseInt(betData[2]);
+                    double risk = Double.parseDouble(betData[3]);
+                    user = new User(login, password);
+                    bet = new Bet(value, risk);
+                }
+            } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                System.out.println("Please enter correct data!");
             }
+            userDao.add(user);
             betDao.add(bet);
-            System.out.println(bet == null ? null : bet.toString());
+            if (user != null && bet != null) {
+                System.out.println(user.toString() + "\n" + bet.toString());
+            }
         }
     }
 }
