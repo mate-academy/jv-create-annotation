4a57016efcd20620e47471e1f8e84a56fb3ea007
MaliukDaria <maliuk.dasha@gmail.com>
2020-09-01 11:39:10 +0300

    added UserDao field in ConsoleHandler class, resolved problem with zero risk or value

--- src/main/java/core/basesyntax/controller/ConsoleHandler.java
+++ src/main/java/core/basesyntax/controller/ConsoleHandler.java
@@ -1,30 +1,38 @@
+
 package core.basesyntax.controller;
 
 import core.basesyntax.dao.BetDao;
 import core.basesyntax.dao.BetDaoImpl;
+import core.basesyntax.dao.UserDao;
+import core.basesyntax.dao.UserDaoImpl;
 import core.basesyntax.model.Bet;
 import java.util.Scanner;
 
 public class ConsoleHandler {
     BetDao betDao = new BetDaoImpl();
+    UserDao userDao = new UserDaoImpl();
 
     public void handle() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
+            boolean isInvalid = true;
             int value = 0;
-            while (value == 0) {
+            while (isInvalid) {
                 System.out.println("Enter value:");
                 try {
                     value = Integer.parseInt(scanner.nextLine());
+                    isInvalid = false;
                 } catch (NumberFormatException e) {
                     System.out.println("Invalid data. Enter number");
                 }
             }
             double risk = 0;
-            while (risk == 0) {
+            isInvalid = true;
+            while (isInvalid) {
                 System.out.println("Enter risk:");
                 try {
                     risk = Double.parseDouble(scanner.nextLine());
+                    isInvalid = false;
                 } catch (NumberFormatException e) {
                     System.out.println("Invalid data. Enter number");
                 }
