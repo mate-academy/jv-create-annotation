6570f27c62c18cc9b6f7ef2bf85e201738a16c59
Artem Vremere <vremere_a@ukr.net>
2020-09-01 09:41:08 +0300

    added exception ArrayIndexOutOfBoundsException moved up  betDao.add(bet);

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,23 +1,11 @@
 package core.basesyntax;
 
 import core.basesyntax.controller.ConsoleHandler;
-import core.basesyntax.dao.BetDao;
-import core.basesyntax.dao.OrderDao;
-import core.basesyntax.factory.Factory;
-import core.basesyntax.lib.Injector;
-
-import java.lang.reflect.InvocationTargetException;
 
 public class Main {
-    public static void main(String[] args) throws InvocationTargetException,
-            NoSuchMethodException, InstantiationException, IllegalAccessException {
-        ConsoleHandler handler = (ConsoleHandler) Injector
-                .getInstance(ConsoleHandler.class);
-        System.out.println("Please choose type of data: bet or order");
+    public static void main(String[] args) {
+        ConsoleHandler handler = new ConsoleHandler();
+        System.out.println("Please, enter value and risk, for your bet");
         handler.handle();
-        BetDao betDao = Factory.getBetDao();
-        System.out.println("All bets: " + betDao.addAll());
-        OrderDao orderDao = Factory.getOrderDao();
-        System.out.println("All orders: " + orderDao.addAll());
     }
 }
--- src/main/java/core/basesyntax/dao/OrderDaoImpl.java
+++ src/main/java/core/basesyntax/dao/OrderDaoImpl.java
@@ -1,11 +1,9 @@
 package core.basesyntax.dao;
 
 import core.basesyntax.db.Storage;
-import core.basesyntax.lib.Dao;
 import core.basesyntax.model.Order;
 import java.util.List;
 
-@Dao
 public class OrderDaoImpl implements OrderDao {
     @Override
     public void add(Order order) {
