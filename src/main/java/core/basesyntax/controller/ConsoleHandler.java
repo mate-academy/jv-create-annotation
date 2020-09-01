2f432c1d934abed28eca06cd64b2e89c79368567
Victoria-Moiseienko <teasevic@gmail.com>
2020-09-01 14:41:51 +0300

    update after review: catch of IndexOutOfBoundsException, access modifier was added

--- src/main/java/core/basesyntax/controller/ConsoleHandler.java
+++ src/main/java/core/basesyntax/controller/ConsoleHandler.java
@@ -6,7 +6,7 @@ import core.basesyntax.model.Bet;
 import java.util.Scanner;
 
 public class ConsoleHandler {
-    BetDao betDao = new BetDaoImpl();
+    private BetDao betDao = new BetDaoImpl();
 
     public void handle() {
         Scanner scanner = new Scanner(System.in);
@@ -23,13 +23,16 @@ public class ConsoleHandler {
                 int value = Integer.parseInt(betData[0]);
                 double risk = Double.parseDouble(betData[1]);
                 bet = new Bet(value, risk);
-
             } catch (NumberFormatException e) {
-                System.out.println("Input correct data, please");
+                System.out.println("Input correct data, please. Waiting for numbers");
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Input correct data, please. Waiting for two numbers");
             }
 
-            betDao.add(bet);
-            System.out.println(bet == null ? null : bet.toString());
+            if (bet != null) {
+                betDao.add(bet);
+                System.out.println(bet.toString());
+            }
         }
     }
 }
