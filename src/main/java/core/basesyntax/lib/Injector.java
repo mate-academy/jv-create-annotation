d0c7a5fcb48fb28dfc76a91e8ef7b04dd1f897a7
romanHreits <romagreic@gmail.com>
2020-09-02 13:35:34 +0300

    added changes to check in Injector class

--- src/main/java/core/basesyntax/lib/Injector.java
+++ src/main/java/core/basesyntax/lib/Injector.java
@@ -1,9 +1,7 @@
 package core.basesyntax.lib;
 
 import core.basesyntax.dao.BetDao;
-import core.basesyntax.dao.BetDaoImpl;
 import core.basesyntax.dao.UserDao;
-import core.basesyntax.dao.UserDaoImpl;
 import core.basesyntax.exception.NoSuchInjectionObjectException;
 import core.basesyntax.factory.BetDaoFactory;
 import core.basesyntax.factory.UserDaoFactory;
@@ -22,10 +20,10 @@ public class Injector {
             if (field.getAnnotation(Inject.class) != null) {
                 field.setAccessible(true);
                 if (field.getType().equals(UserDao.class)
-                        && UserDaoImpl.class.isAnnotationPresent(Dao.class)) {
+                        && UserDaoFactory.getInstance().getClass().isAnnotationPresent(Dao.class)) {
                     field.set(instance, UserDaoFactory.getInstance());
                 } else if (field.getType().equals(BetDao.class)
-                        && BetDaoImpl.class.isAnnotationPresent(Dao.class)) {
+                        && BetDaoFactory.getInstance().getClass().isAnnotationPresent(Dao.class)) {
                     field.set(instance, BetDaoFactory.getInstance());
                 } else {
                     throw new NoSuchInjectionObjectException("The required injection "
