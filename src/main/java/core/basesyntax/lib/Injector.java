1e9ddeb3ccd87dba578998482b64a39a14e9662d
Kostian <pyteec.ua@i.ua>
2020-09-02 12:04:06 +0300

    shorted my code using if-else if-else construction, removed code(setting) duplication

--- src/main/java/core/basesyntax/lib/Injector.java
+++ src/main/java/core/basesyntax/lib/Injector.java
@@ -22,21 +22,17 @@ public class Injector {
         for (Field field : declaredFields) {
             if (field.getAnnotation(Inject.class) != null) {
                 field.setAccessible(true);
-                if (field.getType().equals(BetDao.class)) {
-                    if (betDaoImplClass.isAnnotationPresent(Dao.class)) {
-                        field.set(instance, Factory.getBetDao());
-                    } else {
-                        throw new NoAnnotationFoundException("Такої анотації не знайдено");
-                    }
+                if (field.getType().equals(BetDao.class)
+                        && betDaoImplClass.isAnnotationPresent(Dao.class)) {
+                    field.set(instance, Factory.getBetDao());
+                } else {
+                    throw new NoAnnotationFoundException("Такої анотації не знайдено");
                 }
-
-                if (field.getType().equals(UserDao.class)) {
+                if (field.getType().equals(UserDao.class)
+                        && userDaoImplClass.isAnnotationPresent(Dao.class)) {
                     field.set(instance, Factory.getUserDao());
-                    if (userDaoImplClass.isAnnotationPresent(Dao.class)) {
-                        field.set(instance, Factory.getUserDao());
-                    } else {
-                        throw new NoAnnotationFoundException("Такої анотації не знайдено");
-                    }
+                } else {
+                    throw new NoAnnotationFoundException("Такої анотації не знайдено");
                 }
             }
         }
