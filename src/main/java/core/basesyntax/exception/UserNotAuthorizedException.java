c01aec3992035f18c81a26dd1dbf52766320e032
aleksandr.hrankin <aleksandr.hrankin@gmail.com>
2020-09-02 15:57:11 +0300

    Remove exception

--- src/main/java/core/basesyntax/exception/UserNotAuthorizedException.java
+++
@@ -1,7 +0,0 @@
-package core.basesyntax.exception;
-
-public class UserNotAuthorizedException extends Exception {
-    public UserNotAuthorizedException(String message) {
-        super(message);
-    }
-}
