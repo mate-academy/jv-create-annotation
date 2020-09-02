7edb547f3faaeb4905af97ebc4096a1521bf1455
Roman <matviichuk.office@gmail.com>
2020-09-02 13:55:10 +0300

    created new branch for hw23 fixed naming classes and chose the right exception fixed getInstance() and removed unneeded static method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
 import core.basesyntax.controller.ConsoleHandler;
+import core.basesyntax.exception.DaoAnnotationNotFound;
+import core.basesyntax.lib.Injector;
+import java.lang.reflect.InvocationTargetException;
 
 public class Main {
-    public static void main(String[] args) {
-        ConsoleHandler handler = new ConsoleHandler();
+    public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException,
+            InstantiationException, DaoAnnotationNotFound, IllegalAccessException {
+        ConsoleHandler handler = (ConsoleHandler) Injector.getInstance(ConsoleHandler.class);
         System.out.println("Enter yours login, password, value, risk. Push ENTER! "
                 + "After should write GO");
         handler.handle();
--- src/main/java/core/basesyntax/controller/ConsoleHandler.java
+++ src/main/java/core/basesyntax/controller/ConsoleHandler.java
@@ -1,16 +1,17 @@
 package core.basesyntax.controller;
 
 import core.basesyntax.dao.BetDao;
-import core.basesyntax.dao.BetDaoImpl;
 import core.basesyntax.dao.UserDao;
-import core.basesyntax.dao.UserDaoImpl;
+import core.basesyntax.lib.Inject;
 import core.basesyntax.model.Bet;
 import core.basesyntax.model.User;
 import java.util.Scanner;
 
 public class ConsoleHandler {
-    private UserDao userDao = new UserDaoImpl();
-    private BetDao betDao = new BetDaoImpl();
+    @Inject
+    private UserDao userDao;
+    @Inject
+    private BetDao betDao;
 
     public void handle() {
         Scanner scanner = new Scanner(System.in);
