689eab4f574390bd4053eccb78d94ea5fdad2e25
MaliukDaria <maliuk.dasha@gmail.com>
2020-09-02 14:33:02 +0300

    simplified checks in Injector class, renamed custom Exception

--- src/main/java/core/basesyntax/exception/NoRequiredAnnotationException.java
+++
@@ -1,7 +0,0 @@
-package core.basesyntax.exception;
-
-public class NoRequiredAnnotationException extends RuntimeException {
-    public NoRequiredAnnotationException(String message) {
-        super(message);
-    }
-}
--- src/main/java/core/basesyntax/lib/Injector.java
+++ src/main/java/core/basesyntax/lib/Injector.java
@@ -2,7 +2,7 @@ package core.basesyntax.lib;
 
 import core.basesyntax.dao.BetDao;
 import core.basesyntax.dao.UserDao;
-import core.basesyntax.exception.NoRequiredAnnotationException;
+import core.basesyntax.exception.NoImplementationException;
 import core.basesyntax.factory.BetDaoFactory;
 import core.basesyntax.factory.UserDaoFactory;
 import java.lang.reflect.Constructor;
@@ -18,19 +18,18 @@ public class Injector {
         for (Field field : declaredFields) {
             if (field.getAnnotation(Inject.class) != null) {
                 BetDao betDao = BetDaoFactory.getBetDao();
+                UserDao userDao = UserDaoFactory.getUserDao();
+                field.setAccessible(true);
                 if (field.getType().equals(BetDao.class)
                         && betDao.getClass().isAnnotationPresent(Dao.class)) {
-                    field.setAccessible(true);
                     field.set(instance, betDao);
                 } else {
-                    UserDao userDao = UserDaoFactory.getUserDao();
                     if (field.getType().equals(UserDao.class)
                             && userDao.getClass().isAnnotationPresent(Dao.class)) {
-                        field.setAccessible(true);
                         field.set(instance, userDao);
                     } else {
-                        throw new NoRequiredAnnotationException(String.format(
-                                "The %s class has no @Dao annotation", field.getType().getName()));
+                        throw new NoImplementationException(field.getName()
+                                + " doesn't have an implementation that can be injected");
                     }
                 }
             }
