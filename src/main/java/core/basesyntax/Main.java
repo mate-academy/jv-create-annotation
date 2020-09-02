8ff584f8e6fb778fb8dbd61da6c52eb1e3cec753
Taras <46975882+glones@users.noreply.github.com>
2020-09-02 13:33:17 +0300

    Update Main.java

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,9 +4,6 @@ import core.basesyntax.controller.ConsoleHandler;
 import core.basesyntax.lib.Injector;
 import java.lang.reflect.InvocationTargetException;
 
-/**
- * Feel free to remove this class and create your own.
- */
 public class Main {
     public static void main(String[] args) throws InvocationTargetException,
             NoSuchMethodException, InstantiationException, IllegalAccessException {
