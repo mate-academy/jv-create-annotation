67b086be6f2584e1cbf10395bfe938187dc2b9d3
ivan-lytvynets <ivlitvinets@gmail.com>
2021-01-04 21:01:25 +0200

    implemented Factory

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,6 +1,8 @@
 package core.basesyntax;
 
 import core.basesyntax.controller.ConsoleHandler;
+import core.basesyntax.dao.BetDao;
+import core.basesyntax.factory.Factory;
 import core.basesyntax.lib.Injector;
 
 import java.lang.reflect.InvocationTargetException;
@@ -11,5 +13,8 @@ public class Main {
         ConsoleHandler handler = (ConsoleHandler) Injector.getInstance(ConsoleHandler.class);
         System.out.println("Enter value and risk for your bet");
         handler.handle();
+
+        BetDao betDao = Factory.getBetDao();
+        System.out.println("all bets: " + betDao.getAll());
     }
 }
--- src/main/java/core/basesyntax/lib/Injector.java
+++ src/main/java/core/basesyntax/lib/Injector.java
@@ -1,6 +1,8 @@
 package core.basesyntax.lib;
 
 import core.basesyntax.dao.BetDaoImpl;
+import core.basesyntax.factory.Factory;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -17,7 +19,7 @@ public class Injector {
         for (Field field : declaredFields) {
             if (field.getAnnotation(Inject.class) != null) {
                 field.setAccessible(true);
-                field.set(instance, new BetDaoImpl());
+                field.set(instance, Factory.getBetDao());
             }
         }
         return instance;
