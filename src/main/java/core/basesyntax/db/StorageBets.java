4a77a2cef7d786496b3d24ef04881971e17b80e1
ivlytvynets <47257443+ivlytvynets@users.noreply.github.com>
2021-01-05 12:17:08 +0200

    Revert "Hw 01, added solution"

--- src/main/java/core/basesyntax/db/StorageBets.java
+++
@@ -1,9 +0,0 @@
-package core.basesyntax.db;
-
-import core.basesyntax.model.Bet;
-import java.util.ArrayList;
-import java.util.List;
-
-public class StorageBets {
-    public static final List<Bet> bets = new ArrayList<>();
-}
