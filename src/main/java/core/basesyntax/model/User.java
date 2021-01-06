c6d1cf1a7dc7cd71bc92fc50a3c4bf0229dd79fe
Vadim <Vedroidua@gmail.com>
2021-01-06 17:50:53 +0200

    Added User and UserDao

---
+++ src/main/java/core/basesyntax/model/User.java
@@ -0,0 +1,38 @@
+package core.basesyntax.model;
+
+import java.util.List;
+
+public class User {
+    private String name;
+    private String email;
+    private List<Bet> bets;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Bet> getBets() {
+        return bets;
+    }
+
+    public void setBets(List<Bet> bets) {
+        this.bets = bets;
+    }
+}
