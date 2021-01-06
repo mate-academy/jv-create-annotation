fe64b9c8bf4753ee5e404fe48ac937138ffb9268
engineer027 <dp270597dma@gmail.com>
2021-01-06 19:40:25 +0200

    added solutions

---
+++ src/main/java/core/basesyntax/dao/FruitDaoImpl.java
@@ -0,0 +1,17 @@
+package core.basesyntax.dao;
+
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.Fruit;
+import java.util.List;
+
+public class FruitDaoImpl implements FruitDao {
+    @Override
+    public void add(Fruit fruit) {
+        Storage.fruits.add(fruit);
+    }
+
+    @Override
+    public List<Fruit> getAll() {
+        return Storage.fruits;
+    }
+}
