1b27c125f34d2cce0e63ff3826e6f9f3d568faa1
Alex Korzun <koleksandr4@gmail.com>
2021-01-06 15:34:27 +0200

    created console betting app

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+import core.basesyntax.controller.ConsoleHandler;
+
+public class Main {
+    public static void main(String[] args) {
+        ConsoleHandler handler = new ConsoleHandler();
+        System.out.println("Print 'value, risk' for your bet." +
+                "For quit type 'quit'.");
+        handler.handle();
+    }
+}
