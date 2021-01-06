d29f7c72296f7852d92be10d50afd1c28c703ca5
gigbat <b.kostevich@gmail.com>
2021-01-06 16:12:00 +0200

    Removed imports

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,15 +1,8 @@
 package core.basesyntax;
 
-import core.basesyntax.dao.BetDao;
-import core.basesyntax.dao.BetDaoImpl;
-import core.basesyntax.dao.FruitDao;
-import core.basesyntax.dao.FruitDaoImpl;
 import core.basesyntax.handler.BetConsoleHandler;
 import core.basesyntax.handler.FruitConsoleHandler;
 import core.basesyntax.handler.Handler;
-import core.basesyntax.model.Bet;
-import core.basesyntax.model.Fruit;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
