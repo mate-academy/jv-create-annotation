9d7762263032a5a8374251db2804b77ee7d2a9f0
antoniukivan <ivan.antonuk1992@gmail.com>
2021-01-07 17:12:40 +0200

    add class UserDaoImpl with override methods to dao package

--- src/main/java/core/basesyntax/dao/UserDaoImpl.java
+++ src/main/java/core/basesyntax/dao/UserDaoImpl.java
@@ -1,5 +1,19 @@
 package core.basesyntax.dao;
 
-public class UserDaoImpl implements  {
+import core.basesyntax.db.Storage;
+import core.basesyntax.model.User;
 
+import java.util.List;
+
+public class UserDaoImpl implements UserDao {
+
+    @Override
+    public void add(User user) {
+        Storage.users.add(user);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return Storage.users;
+    }
 }
