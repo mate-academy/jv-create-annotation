ef7c0acd84e3d033344096fdb2d64e899afdebe0
antonpanchishko <PanchishkoMayor@gmail.com>
2021-01-07 23:50:38 +0200

    2) add new model class

--- src/main/java/core/basesyntax/controler/ConsoleHandler.java
+++ src/main/java/core/basesyntax/controler/ConsoleHandler.java
@@ -2,11 +2,15 @@ package core.basesyntax.controler;
 
 import core.basesyntax.dao.BetDao;
 import core.basesyntax.dao.BetDaoImpl;
+import core.basesyntax.dao.UserDao;
+import core.basesyntax.dao.UserDaoImpl;
 import core.basesyntax.model.Bet;
+import core.basesyntax.model.User;
 import java.util.Scanner;
 
 public class ConsoleHandler {
     BetDao betDao = new BetDaoImpl();
+    UserDao userDao = new UserDaoImpl();
 
     public void betHandler() {
         Scanner scanner = new Scanner(System.in);
@@ -27,6 +31,27 @@ public class ConsoleHandler {
             }
             betDao.addBet(bet);
             System.out.println(bet);
+        }
+    }
+
+    public void userHandler() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equalsIgnoreCase("q")) {
+                return;
+            }
+            User user = null;
+            try {
+                String[] split = command.split(" ");
+                String name = split[0];
+                String lastname = split[1];
+                user = new User(name, lastname);
+            } catch (NumberFormatException e) {
+                System.out.println("Будь-ласка введіть коректні дані");
+            }
+            userDao.addUser(user);
+            System.out.println();
         }
     }
 }
