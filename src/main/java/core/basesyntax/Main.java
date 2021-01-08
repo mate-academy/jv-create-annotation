556eeaf9822f716b44c4c838f37cfd5d58d28951
gigbat <b.kostevich@gmail.com>
2021-01-08 22:13:49 +0200

    Realized own annotations

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,12 +1,22 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.NoAnnotationsExceptions;
 import core.basesyntax.handler.ConsoleHandler;
 import core.basesyntax.handler.Handler;
+import core.basesyntax.lib.Injector;
+import java.lang.reflect.InvocationTargetException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException,
+            InstantiationException, IllegalAccessException {
         System.out.println("Please, enter 'value' and 'risk' for your bets.");
-        Handler consoleHandler = new ConsoleHandler();
+        Handler consoleHandler = null;
+        try {
+            consoleHandler = (ConsoleHandler) Injector
+                    .getInstance(ConsoleHandler.class);
+        } catch (NoAnnotationsExceptions e) {
+            System.out.println(e.getMessage());
+        }
         consoleHandler.handle();
     }
 }
--- src/main/java/core/basesyntax/handler/ConsoleHandler.java
+++ src/main/java/core/basesyntax/handler/ConsoleHandler.java
@@ -1,12 +1,19 @@
 package core.basesyntax.handler;
 
 import core.basesyntax.dao.BetDao;
-import core.basesyntax.dao.BetDaoImpl;
+import core.basesyntax.dao.FruitDao;
+import core.basesyntax.lib.Dao;
+import core.basesyntax.lib.Inject;
 import core.basesyntax.model.Bet;
 import java.util.Scanner;
 
 public class ConsoleHandler implements Handler {
-    private BetDao betDao = new BetDaoImpl();
+    private static final String EXIT = "q";
+    private static final String SPACE_SEPARATOR = " ";
+    @Inject
+    private BetDao betDao;
+    @Dao
+    private FruitDao fruitDao;
 
     @Override
     public void handle() {
@@ -15,11 +22,11 @@ public class ConsoleHandler implements Handler {
         while (true) {
             Bet bet = null;
             String input = scanner.nextLine();
-            if (input.equalsIgnoreCase("q")) {
+            if (input.equalsIgnoreCase(EXIT)) {
                 return;
             }
             try {
-                String[] parseString = input.split(" ");
+                String[] parseString = input.split(SPACE_SEPARATOR);
                 int value = Integer.parseInt(parseString[0]);
                 double risk = Integer.parseInt(parseString[1]);
 
@@ -28,6 +35,7 @@ public class ConsoleHandler implements Handler {
                     continue;
                 }
                 bet = new Bet(value, risk);
+                System.out.println(bet != null ? bet.toString() : "");
             } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
                 System.out.println("Enter correct values. Your data is - "
                         + (bet != null ? bet.toString() : ""));
