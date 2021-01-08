2211135ba0c6ce1af03a19257419552e9c9cd393
gigbat <b.kostevich@gmail.com>
2021-01-08 23:48:23 +0200

    Refactored code with type and annotation Dao

--- src/main/java/core/basesyntax/dao/FruitDaoImpl.java
+++ src/main/java/core/basesyntax/dao/FruitDaoImpl.java
@@ -1,9 +1,11 @@
 package core.basesyntax.dao;
 
 import core.basesyntax.db.Storage;
+import core.basesyntax.lib.Dao;
 import core.basesyntax.model.Fruit;
 import java.util.List;
 
+@Dao
 public class FruitDaoImpl implements FruitDao {
     @Override
     public void add(Fruit fruit) {
--- src/main/java/core/basesyntax/lib/Dao.java
+++ src/main/java/core/basesyntax/lib/Dao.java
@@ -3,7 +3,7 @@ package core.basesyntax.lib;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-@Retention(RetentionPolicy.RUNTIME)
+@Retention(RetentionPolicy.CLASS)
 public @interface Dao {
 
 }
