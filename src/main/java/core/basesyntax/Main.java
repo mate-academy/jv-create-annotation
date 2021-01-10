4d5328ababfc58db5a3c66442c466cc37576d536
engineer027 <dp270597dma@gmail.com>
2021-01-11 00:23:07 +0200

    fixed maven

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -4,12 +4,11 @@ import core.basesyntax.controller.ConsoleHandler;
 import core.basesyntax.dao.BetDao;
 import core.basesyntax.factory.Factory;
 import core.basesyntax.lib.Injector;
-
 import java.lang.reflect.InvocationTargetException;
 
 public class Main {
-    public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        //ConsoleHandler consoleHandler = new ConsoleHandler();
+    public static void main(String[] args) throws InvocationTargetException,
+            NoSuchMethodException, InstantiationException, IllegalAccessException {
         ConsoleHandler consoleHandler = (ConsoleHandler) Injector.getInstance(ConsoleHandler.class);
         System.out.println("Введіть value та risk ля вашої ставки");
         consoleHandler.handle();
--- src/main/java/core/basesyntax/lib/Injector.java
+++ src/main/java/core/basesyntax/lib/Injector.java
@@ -1,14 +1,12 @@
 package core.basesyntax.lib;
 
-import core.basesyntax.dao.BetDaoImpl;
 import core.basesyntax.factory.Factory;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 
 public class Injector {
-    public  static Object getInstance (Class clazz) throws NoSuchMethodException,
+    public static Object getInstance(Class clazz) throws NoSuchMethodException,
             IllegalAccessException, InvocationTargetException, InstantiationException {
         Constructor constructor = clazz.getDeclaredConstructor();
         Object instance = constructor.newInstance();
