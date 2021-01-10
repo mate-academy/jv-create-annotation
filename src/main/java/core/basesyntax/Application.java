70914059b37a05d93bfb0d39a43d7d81ba203161
Resci <vIsUAl123>
2021-01-10 18:10:29 +0200

    Change logic

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,10 +1,12 @@
 package core.basesyntax;
 
 import core.basesyntax.controller.ConsoleHandler;
+import core.basesyntax.dao.BetDaoImpl;
+import core.basesyntax.dao.UserDaoImpl;
 
 public class Application {
     public static void main(String[] args) {
-        ConsoleHandler consoleHandler = new ConsoleHandler();
+        ConsoleHandler consoleHandler = new ConsoleHandler(new BetDaoImpl(), new UserDaoImpl());
         consoleHandler.handle();
     }
 }
--- src/main/java/core/basesyntax/controller/ConsoleHandler.java
+++ src/main/java/core/basesyntax/controller/ConsoleHandler.java
@@ -1,32 +1,56 @@
 package core.basesyntax.controller;
 
 import core.basesyntax.dao.BetDao;
-import core.basesyntax.dao.BetDaoImpl;
+import core.basesyntax.dao.UserDao;
 import core.basesyntax.model.Bet;
+import core.basesyntax.model.User;
 import java.util.Scanner;
 
 public class ConsoleHandler {
-    BetDao betDao = new BetDaoImpl();
+    private static final String EXIT_COMMAND = "q";
+    private static final String SEPARATOR = " ";
+    private static final int BET_VALUE_INDEX = 0;
+    private static final int BET_RISK_INDEX = 1;
+    private static final int USER_NAME_INDEX = 2;
+    private static final int USER_AGE_INDEX = 3;
+    private BetDao betDao;
+    private UserDao userDao;
+
+    public ConsoleHandler(BetDao betDao, UserDao userDao) {
+        this.betDao = betDao;
+        this.userDao = userDao;
+    }
 
     public void handle() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your data in following format:\n"
+                + "bet_value" + SEPARATOR
+                + "risk" + SEPARATOR
+                + "user_name" + SEPARATOR
+                + "user_age");
         while (true) {
-            System.out.println("Please, enter value and risk for your bet or 'q' for exit");
             String command = scanner.nextLine();
-            if (command.equalsIgnoreCase("q")) {
+            if (command.equalsIgnoreCase(EXIT_COMMAND)) {
                 return;
             }
-            Bet bet = null;
             try {
-                String[] betData = command.split(" ");
-                int value = Integer.parseInt(betData[0]);
-                double risk = Double.parseDouble(betData[1]);
-                bet = new Bet(value, risk);
-            } catch (NumberFormatException e) {
-                System.out.println("Incorrect input, please enter again");
+                String[] betData = command.split(SEPARATOR);
+
+                int value = Integer.parseInt(betData[BET_VALUE_INDEX]);
+                double risk = Double.parseDouble(betData[BET_RISK_INDEX]);
+                Bet bet = new Bet(value, risk);
+
+                String userName = betData[USER_NAME_INDEX];
+                int userAge = Integer.parseInt(betData[USER_AGE_INDEX]);
+                User user = new User(userName, userAge);
+
+                betDao.add(bet);
+                userDao.add(user);
+
+                System.out.println(bet.toString());
+            } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+                System.out.println("Please, enter correct data");
             }
-            betDao.add(bet);
-            System.out.println(bet == null ? null : bet.toString());
         }
     }
 }
