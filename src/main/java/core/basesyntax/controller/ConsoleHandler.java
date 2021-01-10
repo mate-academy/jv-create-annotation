09b3029292a332ff702ea183a292872f297f44cc
Elena Samsonova <elenasamsonova08@gmail.com>
2021-01-10 22:38:36 +0200

    added some checks and removed magic numbers to constant fields

--- src/main/java/core/basesyntax/controller/ConsoleHandler.java
+++ src/main/java/core/basesyntax/controller/ConsoleHandler.java
@@ -9,6 +9,12 @@ import core.basesyntax.model.User;
 import java.util.Scanner;
 
 public class ConsoleHandler {
+    private static final String QUIT_APP = "q";
+    private static final String DIVIDER = " ";
+    private static final int INDEX_VALUE = 0;
+    private static final int INDEX_RISK = 1;
+    private static final int INDEX_NAME = 0;
+    private static final int INDEX_SPORT = 1;
     private final BetDao betDao = new BetDaoImpl();
     private final UserDao userDao = new UserDaoImpl();
 
@@ -16,19 +22,22 @@ public class ConsoleHandler {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String command = scanner.nextLine();
-            if (command.equalsIgnoreCase("q")) {
+            if (command.equalsIgnoreCase(QUIT_APP)) {
                 return;
             }
             Bet bet = null;
             try {
-                String[] betData = command.split(" ");
-                int value = Integer.parseInt(betData[0]);
-                double risk = Double.parseDouble(betData[1]);
+                String[] betData = command.split(DIVIDER);
+                if (betData.length != 2) {
+                    throw new RuntimeException("Incorrect data, expected pair of values");
+                }
+                int value = Integer.parseInt(betData[INDEX_VALUE]);
+                double risk = Double.parseDouble(betData[INDEX_RISK]);
                 bet = new Bet(value, risk);
+                betDao.addBet(bet);
             } catch (NumberFormatException e) {
                 System.out.println("Please, enter correct data:");
             }
-            betDao.addBet(bet);
             System.out.println(bet == null ? null : bet.toString());
         }
     }
@@ -37,17 +46,20 @@ public class ConsoleHandler {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String command = scanner.nextLine();
-            if (command.equalsIgnoreCase("q")) {
+            if (command.equalsIgnoreCase(QUIT_APP)) {
                 System.out.println(userDao.getAll());
                 return;
             }
-            String[] userData = command.split(" ");
-            String name = userData[0];
-            String sport = userData[1];
+            String[] userData = command.split(DIVIDER);
+            if (userData.length != 2) {
+                throw new RuntimeException("Incorrect data, expected pair of values");
+            }
+            String name = userData[INDEX_NAME];
+            String sport = userData[INDEX_SPORT];
             if (name.length() == 0 || sport.length() == 0) {
                 System.out.println("Please, enter correct name and favourite sport");
             } else {
-                User user = new User(name,sport);
+                User user = new User(name, sport);
                 userDao.add(user);
             }
         }
