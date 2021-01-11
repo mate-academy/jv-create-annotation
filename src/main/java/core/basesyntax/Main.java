ca76f6112ea7a7fe509e060f9c80b9aa95e7deeb
gigbat <b.kostevich@gmail.com>
2021-01-11 17:04:39 +0200

    Removed throws custom exception in main method

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,6 +1,5 @@
 package core.basesyntax;
 
-import core.basesyntax.exception.NoAnnotationsExceptions;
 import core.basesyntax.handler.ConsoleHandler;
 import core.basesyntax.handler.Handler;
 import core.basesyntax.lib.Injector;
@@ -8,7 +7,7 @@ import java.lang.reflect.InvocationTargetException;
 
 public class Main {
     public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException,
-            InstantiationException, IllegalAccessException, NoAnnotationsExceptions {
+            InstantiationException, IllegalAccessException {
         System.out.println("Please, enter 'value' and 'risk' for your bets.");
         Handler consoleHandler = (ConsoleHandler) Injector
                 .getInstance(ConsoleHandler.class);
